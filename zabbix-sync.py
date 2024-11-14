09fafbc896f668e26822f54698d9e929ac9fad7f
Daniel Pereira <60305462+danielsilvapereira@users.noreply.github.com>
2024-11-14 19:50:47 -0300

    Update zabbix-sync.py

--- zabbix-sync.py
+++ zabbix-sync.py
@@ -1,10 +1,15 @@
-import time
+#!/usr/bin/env python3
+
+### */5 * * * * /usr/bin/python3 /home/gubit/docker-oxidized/zabbix-sync.py ##
+#./zabbix-sync.py > /dev/null 2>&1
+
+#import time
 from pyzabbix import ZabbixAPI
 
 # Configurações
-ZABBIX_API_URL = "https://seu-zabbix-url"
-API_TOKEN = "seu_token_aqui"
-ROUTER_DB_PATH = "/root/.config/oxidized/router.db"
+ZABBIX_API_URL = "http://URL/api_jsonrpc.php"
+API_TOKEN = ""
+ROUTER_DB_PATH = "/home/gubit/docker-oxidized/oxidized/router.db"
 
 # Conexão com a API do Zabbix usando o token
 zapi = ZabbixAPI(ZABBIX_API_URL)
@@ -19,7 +24,7 @@ def get_hosts_with_tag_and_macros(tag, macro_names):
         selectMacros='extend',
         tags=[{'tag': tag}]
     )
-    
+
     host_info = []
     for host in hosts:
         # Filtra a tag oxidized
@@ -29,7 +34,8 @@ def get_hosts_with_tag_and_macros(tag, macro_names):
                 macros = {macro['macro']: macro['value'] for macro in host['macros'] if macro['macro'] in macro_names}
                 user = macros.get('{$BACKUP.USUARIO}', 'N/A')
                 senha = macros.get('{$BACKUP.SENHA}', 'N/A')
-                host_info.append(f"{host['host']}; {ip}; {host_tag['value']}; {user}; {senha}")
+                porta = macros.get('{$BACKUP.PORTA}', 'N/A')
+                host_info.append(f"{host['host']}:{ip}:{host_tag['value']}:{user}:{senha}:{porta}")
     return host_info
 
 # Função para escrever as informações no arquivo router.db
@@ -39,19 +45,18 @@ def write_to_router_db(data, file_path):
             file.write(line + '\n')
 
 # Nomes das macros a buscar
-macro_names = ['{$BACKUP.USUARIO}', '{$BACKUP.SENHA}']
+macro_names = ['{$BACKUP.USUARIO}', '{$BACKUP.SENHA}', '{$BACKUP.PORTA}']
 
 # Loop contínuo para atualização do arquivo router.db
 try:
     while True:
-        hosts = get_hosts_with_tag_and_macros('oxidized', macro_names)
+        hosts = get_hosts_with_tag_and_macros('backup', macro_names)
         if hosts:
             write_to_router_db(hosts, ROUTER_DB_PATH)
             print("Arquivo router.db atualizado.")
         else:
             print("Nenhum host encontrado com a tag 'oxidized'.")
-        
-        time.sleep(300)  # Espera 5 minutos antes de atualizar novamente
 
+        time.sleep(60)  # Espera 5 minutos antes de atualizar novamente
 except Exception as e:
     print(f"Erro: {e}")
